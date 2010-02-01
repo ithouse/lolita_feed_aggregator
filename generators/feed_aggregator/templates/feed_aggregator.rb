4800ecff732fa8a9fa26b6889365fead278b5806
Gatis Tomsons <gatis@ithouse.lv>
2010-02-01 14:39:05 +0200

    Updated example

--- generators/feed_aggregator/templates/feed_aggregator.rb
+++ generators/feed_aggregator/templates/feed_aggregator.rb
@@ -3,7 +3,7 @@
 #  aggregate(:twitter, "http://twitter.com/statuses/user_timeline.xml?screen_name=envylabs", :limit => 10) do |doc|
 #    (doc/:status).collect{ |item|
 #      {
-#        :created_at => (item/:created_at).inner_html,
+#        :created_at => (item/:created_at).first.inner_html,
 #        :value => item,
 #        :order_nr => (item/:id).first.inner_html
 #      } if (item/:text).inner_html =~ /(#Ruby5|#rails)/
